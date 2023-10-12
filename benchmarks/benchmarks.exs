ef76afaf0496d745d9376a72dfa642885795795d
Rokey <@github.com>
2023-10-12 10:31:26 +0800

    update benchmark

--- benchmarks/benchmarks.exs
+++ benchmarks/benchmarks.exs
@@ -10,12 +10,12 @@ Benchee.run(
         |> elem(1)
       )
     end,
-"pull_global_ids" => fn ->
+    "pull_global_ids" => fn ->
       SimpleHandler.pull_message_ids(1, 1)
     end
   },
   formatters: [
-    {Benchee.Formatters.Console, comparison: false, extended_statistics: true},
+    {Benchee.Formatters.Console, comparison: false, extended_statistics: true}
     # {Benchee.Formatters.HTML, extended_statistics: true, auto_open: false}
   ],
   print: [
